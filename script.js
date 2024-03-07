6eda87d75bdd02ce8a5415f72c27d5824453c8a5
JuanAndrade11 <juanpablo.andradem@gmail.com>
2024-03-07 12:53:40 -0500

    Add logic to play 1 round
    - add beats map to determine winner.
    
    - add round logic.

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-var options = {
+let options = {
     1: "rock",
     2: "paper",
     3: "scissors"
 };
 
+let beats = {
+    "rock" : "scissors",
+    "paper" : "rock",
+    "scissors" : "paper"
+};
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
-}
+};
 
-console.log(options[getComputerChoice()])
+function playRound(playerSelection, computerSelection){
+    let playerBeats = beats[playerSelection];
+    if (playerBeats === computerSelection) {
+        return("You Win! " + playerSelection + " beats " + computerSelection);
+    } else if (playerSelection === computerSelection){
+        return("A boring Tie");
+    } else {
+        return("You Lose! " + computerSelection + " beats " + playerSelection);
+    };
+};
+
+const playerSelection = "rock";
+const computerSelection = options[getComputerChoice()];
+console.log(playRound(playerSelection, computerSelection));
