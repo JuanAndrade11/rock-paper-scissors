669972da3db744f8ad505caa8b6d8f79813e793f
JuanAndrade11 <juanpablo.andradem@gmail.com>
2024-03-07 13:40:25 -0500

    Add logic for first to 3 wins
    - Add a playGame function with a while loop to count up to 3 wins for player or pc.
    
    - Add prompt with lower case to

--- script.js
+++ script.js
@@ -14,17 +14,45 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
 };
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     let playerBeats = beats[playerSelection];
     if (playerBeats === computerSelection) {
-        return("You Win! " + playerSelection + " beats " + computerSelection);
-    } else if (playerSelection === computerSelection){
-        return("A boring Tie");
+        return { result: "You Win! " + playerSelection + " beats " + computerSelection, playerWon: true };
+    } else if (playerSelection === computerSelection) {
+        return { result: "A boring Tie", tie: true };
     } else {
-        return("You Lose! " + computerSelection + " beats " + playerSelection);
+        return { result: "You Lose! " + computerSelection + " beats " + playerSelection, playerWon: false };
     };
 };
 
-const playerSelection = "rock";
-const computerSelection = options[getComputerChoice()];
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+    let playerRounds = 0;
+    let computerRounds = 0;
+    let tiedRounds = 0;
+
+    while (playerRounds < 3 && computerRounds < 3) {
+        let playerSelection = prompt("Type your selection", "rock").toLowerCase();
+        let computerSelection = options[getComputerChoice()];
+        let roundResult = playRound(playerSelection, computerSelection);
+
+        console.log(roundResult.result);
+
+        if (roundResult.playerWon) {
+            playerRounds++;
+        } else if (!roundResult.tie) {
+            computerRounds++;
+        } else {
+            tiedRounds++;
+        };
+
+        console.log("Player Rounds " + playerRounds + " Computer Rounds " + computerRounds + " ties " + tiedRounds);
+    };
+
+    if (playerRounds === 3) {
+        console.log("You Win! " + playerRounds + " to " + computerRounds + " with " + tiedRounds + " ties");
+    } else {
+        console.log("You Lose! " + playerRounds + " to " + computerRounds + " with " + tiedRounds + " ties");
+    };
+};
+
+playGame();
